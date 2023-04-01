946374d6b88816fa3a08a1734bf7dabbc7dc0285
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-04-01 17:33:30 +0000

    update: クリックボードと選択範囲の変換処理を実装

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,70 @@
 import * as vscode from 'vscode';
 import isUrl = require('is-url');
 
+const SUPPORTED_DOC_ID = [
+	"markdown"	// support for markdown only
+]
+
+const showInfomationAndExit = (message:string) => {
+	vscode.window.showInformationMessage("to linkcard: "+message);
+	return
+};
+
+// validate text is Url string
+// or angle bracketed Url string 
+const validateUrl = (text:string) => {
+	// remove 1 character before and after maybe angle bracketed
+	if (text.startsWith("<") && text.endsWith(">")) {
+		text = text.slice(2).slice(0, -1);
+	}
+	return isUrl(text);
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log('Congratulations, your extension "zenn-url-to-linkcard" is now active!');
-
-	let disposable = vscode.commands.registerCommand('zenn-url-to-linkcard.helloWorld', () => {
+	// TODO: 複数行のURL処理に対応する:データのクレンジングをする -> 改行コードで分割、空行の削除、各行のtrim、行ごとの"<>"除去、
+	let fromClipboard = vscode.commands.registerCommand('zenn-url-to-linkcard.fromClipboard', () => {
 		const promise = vscode.env.clipboard.readText();
 		promise.then((text) => {
-			if (isUrl(text)) {
-				console.log(`${text} is URL!`);
+			const entries = text.split('\n');
+			if (!validateUrl(text)) {
+				return showInfomationAndExit(`text in clipboard is not URL.`);
+			}
+			const editor = vscode.window.activeTextEditor;
+			if (editor == null) {
 				return;
 			}
-			console.log(`${text} is not URL...`);
+			const doc = editor.document;
+			if (doc == null || !SUPPORTED_DOC_ID.includes(doc.languageId)) {
+				return showInfomationAndExit(`unsupported document type.`)				
+			}
+			// text is Url
+			editor.edit(builder => {
+				builder.insert(editor.selection.active, `@[card](${text})\n`);
+			});
 		});
-		vscode.window.showInformationMessage('Hello World from zenn-url-to-linkcard!');
 	});
 
-	context.subscriptions.push(disposable);
+	let fromSelection = vscode.commands.registerCommand('zenn-url-to-linkcard.fromSelection', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor == null) {
+			 return;
+		}
+		const doc = editor.document;
+		if (doc == null) {
+			return
+		}
+		const text = doc.getText(editor.selection).trim();
+		if (!validateUrl(text)) {
+			return showInfomationAndExit(`text in clipboard is not URL.`);
+		}
+		// text is Url
+		editor.edit(builder => {
+			builder.insert(editor.selection.active, `@[card](${text})\n`);
+		});
+	});
+
+	context.subscriptions.push(fromClipboard);
+	context.subscriptions.push(fromSelection);
 }
 
 export function deactivate() {}
