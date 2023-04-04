413cc20dd91a03f0e90f136ecd4358a35477d7d9
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-04-04 15:56:36 +0000

    update: i18n対応を取り込み

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
+'use strict';
 import * as vscode from 'vscode';
 import isUrl = require('is-url');
+import { localeMap } from './localeMap';
 
 const SUPPORTED_DOC_ID = [
 	"markdown"	// support for markdown only
 ]
 
 const showInfomationAndExit = (message:string) => {
-	vscode.window.showInformationMessage("to linkcard: "+message);
+	vscode.window.showInformationMessage(message);
 	return
 };
 
@@ -25,9 +27,8 @@ export function activate(context: vscode.ExtensionContext) {
 	let fromClipboard = vscode.commands.registerCommand('zenn-url-to-linkcard.fromClipboard', () => {
 		const promise = vscode.env.clipboard.readText();
 		promise.then((text) => {
-			const entries = text.split('\n');
 			if (!validateUrl(text)) {
-				return showInfomationAndExit(`text in clipboard is not URL.`);
+				return showInfomationAndExit(localeMap("showInformationMessage.selectionTextIsNotUrl"));
 			}
 			const editor = vscode.window.activeTextEditor;
 			if (editor == null) {
@@ -35,7 +36,7 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			const doc = editor.document;
 			if (doc == null || !SUPPORTED_DOC_ID.includes(doc.languageId)) {
-				return showInfomationAndExit(`unsupported document type.`)				
+				return showInfomationAndExit(localeMap("showInformationMessage.unsupportedDocumentType"))				
 			}
 			// text is Url
 			editor.edit(builder => {
@@ -55,11 +56,10 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		const text = doc.getText(editor.selection).trim();
 		if (!validateUrl(text)) {
-			return showInfomationAndExit(`text in clipboard is not URL.`);
+			return showInfomationAndExit(localeMap("showInformationMessage.selectionTextIsNotUrl"));
 		}
 		// text is Url
 		editor.edit(builder => {
-			// FIXME: selectionの末端にinsertしてしまう -> selection範囲をreplaceする
 			builder.replace(editor.selection, `@[card](${text})\n`);
 		});
 	});
